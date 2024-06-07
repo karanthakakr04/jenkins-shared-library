dae6060062c766eaa8aa869284f4891e3d1a13f0
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-07 00:28:10 -0400

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -5,49 +5,47 @@ def call(Map pipelineParams) {
     IMAGE_TAG = "${env.IMAGE_VERSION}"
   }
 
-  stages {
-    stage('Increment Version') {
-      steps {
-        script {
-          def versioningStage = new org.example.VersioningStage()
-          versioningStage()
-        }
+  stage('Increment Version') {
+    steps {
+      script {
+        def versioningStage = new org.example.VersioningStage()
+        versioningStage()
       }
     }
+  }
 
-    stage('Run Tests') {
-      steps {
-        script {
-          def testStage = new org.example.TestStage()
-          testStage()
-        }
+  stage('Run Tests') {
+    steps {
+      script {
+        def testStage = new org.example.TestStage()
+        testStage()
       }
     }
+  }
 
-    stage('Build Image') {
-      steps {
-        script {
-          def buildStage = new org.example.BuildStage()
-          buildStage(pipelineParams.dockerhubUsername, pipelineParams.dockerhubRepo, env.IMAGE_TAG)
-        }
+  stage('Build Image') {
+    steps {
+      script {
+        def buildStage = new org.example.BuildStage()
+        buildStage(pipelineParams.dockerhubUsername, pipelineParams.dockerhubRepo, env.IMAGE_TAG)
       }
     }
+  }
 
-    stage('Deploy') {
-      steps {
-        script {
-          def deployStage = new org.example.DeployStage()
-          deployStage(pipelineParams.dockerhubUsername, pipelineParams.dockerhubRepo, env.IMAGE_TAG)
-        }
+  stage('Deploy') {
+    steps {
+      script {
+        def deployStage = new org.example.DeployStage()
+        deployStage(pipelineParams.dockerhubUsername, pipelineParams.dockerhubRepo, env.IMAGE_TAG)
       }
     }
+  }
 
-    stage('Commit Version') {
-      steps {
-        script {
-          def commitStage = new org.example.CommitStage()
-          commitStage(env.IMAGE_TAG, pipelineParams.githubRepoUrl)
-        }
+  stage('Commit Version') {
+    steps {
+      script {
+        def commitStage = new org.example.CommitStage()
+        commitStage(env.IMAGE_TAG, pipelineParams.githubRepoUrl)
       }
     }
   }
