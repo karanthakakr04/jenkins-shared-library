fb013515dd1645c2578fa2563df605eff35c54fd
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-06 20:28:09 -0400

    Update VersioningStage.groovy

--- src/org/example/VersioningStage.groovy
+++ src/org/example/VersioningStage.groovy
@@ -4,20 +4,24 @@ package org.example
 
 def call(String versionIncrement) {
   echo "Incrementing application version with ${versionIncrement}"
-  dir('app') {
-    def versionType = input(
-      id: 'versionType',
-      message: 'Select the version increment type:',
-      ok: 'Increment',
-      parameters: [
-        choice(name: 'type', choices: ['patch', 'minor', 'major'], description: 'Version increment type')
-      ]
-    )
-    sh "npm version ${versionType}"
-    def packageJson = readJSON file: 'package.json'
-    def appVersion = packageJson.version
-    def buildNumber = env.BUILD_NUMBER
-    def imageVersion = "${appVersion}-${buildNumber}"
-    env.IMAGE_VERSION = imageVersion
+  dir('8 - Build Automation & CI-CD with Jenkins/jenkins-exercises/app') {
+    if (fileExists('package.json')) {
+      def versionType = input(
+        id: 'versionType',
+        message: 'Select the version increment type:',
+        ok: 'Increment',
+        parameters: [
+          choice(name: 'type', choices: ['patch', 'minor', 'major'], description: 'Version increment type')
+        ]
+      )
+      sh "npm version ${versionType}"
+      def packageJson = readJSON file: 'package.json'
+      def appVersion = packageJson.version
+      def buildNumber = env.BUILD_NUMBER
+      def imageVersion = "${appVersion}-${buildNumber}"
+      env.IMAGE_VERSION = imageVersion
+    } else {
+      error "package.json file not found in the app directory"
+    }
   }
 }
