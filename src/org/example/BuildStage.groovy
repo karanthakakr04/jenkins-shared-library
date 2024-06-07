8fb9564c212057bbb327e320126a53ca5bf28628
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-06 20:26:53 -0400

    Update BuildStage.groovy

--- src/org/example/BuildStage.groovy
+++ src/org/example/BuildStage.groovy
@@ -4,5 +4,7 @@ package org.example
 
 def call(String dockerhubUsername, String dockerhubRepo, String imageTag) {
   echo "Building Docker image ${dockerhubUsername}/${dockerhubRepo}:${imageTag}"
-  sh "docker build -t ${dockerhubUsername}/${dockerhubRepo}:${imageTag} ."
+  dir('8 - Build Automation & CI-CD with Jenkins/jenkins-exercises') {
+    sh "docker build -t ${dockerhubUsername}/${dockerhubRepo}:${imageTag} -f Dockerfile ."
+  }
 }
