970bbc20f5e5f94675838a4a545cc87cea6fdb66
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-06 20:27:28 -0400

    Update DeployStage.groovy

--- src/org/example/DeployStage.groovy
+++ src/org/example/DeployStage.groovy
@@ -4,8 +4,10 @@ package org.example
 
 def call(String dockerhubUsername, String dockerhubRepo, String imageTag) {
   echo "Pushing Docker image ${dockerhubUsername}/${dockerhubRepo}:${imageTag}"
-  withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-    sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-    sh "docker push ${dockerhubUsername}/${dockerhubRepo}:${imageTag}"
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-access', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+    sh '''
+      echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+      docker push ${dockerhubUsername}/${dockerhubRepo}:${imageTag}
+    '''
   }
 }
