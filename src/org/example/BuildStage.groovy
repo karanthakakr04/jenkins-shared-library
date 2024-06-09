60f5a535ce87ffdd9a5e247b7b6185258fbe70a1
Karan Thakkar <thakkar.karan04@gmail.com>
2024-06-09 12:41:43 -0400

    Update BuildStage.groovy

--- src/org/example/BuildStage.groovy
+++ src/org/example/BuildStage.groovy
@@ -3,7 +3,7 @@
 package org.example
 
 def call(String dockerhubUsername, String dockerhubRepo, String imageTag) {
-  echo "Building Docker image for repository: ${dockerhubRepo}"
+  echo 'Build the Docker image with the incremented version...'
   dir('8 - Build Automation & CI-CD with Jenkins/jenkins-exercises') {
     sh "docker build -t ${dockerhubUsername}/${dockerhubRepo}:${imageTag} -f Dockerfile ."
   }
